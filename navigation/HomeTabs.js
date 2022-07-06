63bf384d6dd0feed307fe483ce7a7b65b1a878db
Andres Clavijo <andres.clavijo@hahn-softwareentwicklung.de>
2022-07-05 23:30:57 -0500

    Added font icons

--- navigation/HomeTabs.js
+++ navigation/HomeTabs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Dashboard from '../components/dashboard/Dashboard';
 import Settings from '../components/common/Settings';
-import { MaterialCommunityIcons } from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons  from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,12 +11,12 @@ export default function HomeTabs()
     return (
         <Tab.Navigator>
           <Tab.Screen name="Home" component={Dashboard} options={{ headerShown: false, tabBarIcon: ({color}) => (<MaterialCommunityIcons name="home" color={color} size={26} />) }}/>
-          <Tab.Screen name="Settings" component={Settings} options={{ headerShown: false }}/>
-          <Tab.Screen name="Settings1" component={Settings} options={{ headerShown: false }}/>
-          <Tab.Screen name="Settings2" component={Settings} options={{ headerShown: false }}/>
-          <Tab.Screen name="Settings3" component={Settings} options={{ headerShown: false }}/>
-          <Tab.Screen name="Settings4" component={Settings} options={{ headerShown: false }}/>
-          <Tab.Screen name="Settings5" component={Settings} options={{ headerShown: false }}/>
+          <Tab.Screen name="Settings" component={Settings} options={{ headerShown: false, tabBarIcon: ({color}) => (<MaterialCommunityIcons name="gear" color={color} size={26} />)  }}/>
+          <Tab.Screen name="Settings1" component={Settings} options={{ headerShown: false, tabBarIcon: ({color}) => (<MaterialCommunityIcons name="user" color={color} size={26} />) }}/>
+          <Tab.Screen name="Settings2" component={Settings} options={{ headerShown: false, tabBarIcon: ({color}) => (<MaterialCommunityIcons name="cloud" color={color} size={26} />) }}/>
+          <Tab.Screen name="Settings3" component={Settings} options={{ headerShown: false, tabBarIcon: ({color}) => (<MaterialCommunityIcons name="pencil" color={color} size={26} />) }}/>
+          <Tab.Screen name="Settings4" component={Settings} options={{ headerShown: false, tabBarIcon: ({color}) => (<MaterialCommunityIcons name="send" color={color} size={26} />) }}/>
+          <Tab.Screen name="Settings5" component={Settings} options={{ headerShown: false, tabBarIcon: ({color}) => (<MaterialCommunityIcons name="book" color={color} size={26} />) }}/>
         </Tab.Navigator>
       );
 }
