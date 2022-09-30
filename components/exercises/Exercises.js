16c4f4431a38c4568c27b12bea1d1b09fff0e8c7
Andres Clavijo <andres.clavijo@hahn-softwareentwicklung.de>
2022-09-29 23:39:20 -0500

    Adjusted exercise and todayTraining view

--- components/exercises/Exercises.js
+++ components/exercises/Exercises.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { StyleSheet, View, FlatList, Image, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { StyleSheet, View, FlatList, Text } from "react-native";
+import { Button, Card } from "react-native-elements";
+import { Divider } from "react-native-elements/dist/divider/Divider";
 import { Title } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import FontAwesome from 'react-native-vector-icons/FontAwesome5';
 
 export default function Exercises(props)
 {
-
+    const [isFavorite, setFavorite] = useState(false);
     const DATA = [
         {
             id: 1,
@@ -44,25 +45,73 @@ export default function Exercises(props)
     const renderItem = ({item}) => (
         <View>
             <Card containerStyle={styles.cardContainer}>
-                <Card.Title h4 style={styles.cardTitle}>{item.name}</Card.Title>
+                <Card.Title h4 style={{alignSelf: 'center' }}>
+                    <View style={styles.cardTitle}>
+                        <View style={{marginRight: 10}}>
+                            <FontAwesome name="star" color={'orange'} size={25} solid={isFavorite} onPress={()=> setFavorite(!isFavorite)}/>
+                        </View>
+                        <Title>{item.name}</Title>
+                    </View>                    
+                </Card.Title>
                 <Card.Image
                     style={styles.image}
                     source={{
                         uri: item.image
                     }}
                 />
-                <View style={{flexDirection: "row", alignItems: "baseline", marginTop: 10}}>
-                    <FontAwesome name="walking" color={'black'} style={styles.muscleItemIcon} size={20} solid/>
-                    <Text style={{fontSize: 16}}>Muscles to work</Text>
+                <View style={{borderStyle: 'solid', borderWidth: 1, borderTopWidth: 0, borderBottomLeftRadius: 5, borderBottomRightRadius: 5,borderColor: '#9B9A99', borderTopColor: 'none', padding: 5, marginTop: 10}}>
+                    <View style={{flexDirection: "row", alignItems: "baseline", marginTop: 10}}>
+                        <FontAwesome name="tags" color={'green'} style={styles.muscleItemIcon} size={14} solid/>
+                        <Text style={{fontSize: 16}}>Set of muscles</Text>
+                    </View>
+                    <Divider/>
+                    <View style={styles.musclesContainer}>
+                        {item.muscles.map((muscle) => {
+                            return (
+                            <View style={styles.muscleContainer}>
+                                <FontAwesome name="check-circle" color={'green'} style={styles.muscleItemIcon} size={15} solid/>
+                                <Text>{muscle}</Text>
+                            </View>)
+                        })}
+                    </View>
                 </View>
-                <View style={styles.musclesContainer}>
-                    {item.muscles.map((muscle) => {
-                        return (
-                        <View style={styles.muscleContainer}>
-                            <FontAwesome name="check-circle" color={'green'} style={styles.muscleItemIcon} size={15} solid/>
-                            <Text>{muscle}</Text>
-                        </View>)
-                    })}
+
+                <View style={{borderStyle: 'solid', borderWidth: 1, borderTopWidth: 0, borderBottomLeftRadius: 5, borderBottomRightRadius: 5, borderColor: '#9B9A99', borderTopColor: 'none', padding: 5, marginTop: 10}}>
+                    <View style={{flexDirection: "row", alignItems: "baseline", marginTop: 10}}>
+                        <FontAwesome name="tasks" color={'green'} style={styles.muscleItemIcon} size={18} solid/>
+                        <Text style={{fontSize: 16}}>Actions</Text>
+                        </View>
+                    <Divider/>
+                    <View style={{width: 200, alignSelf: 'center', height: 90, justifyContent: 'space-between', marginTop: 5, paddingBottom: 5}}>
+                        <Button 
+                            icon={
+                                <FontAwesome
+                                    name="plus-circle"
+                                    solid
+                                    size={19}
+                                    style={{marginRight:5}}
+                                    color='#FDB10E'
+                                />
+                            }
+                            title='Add to your routine'
+                            type="outline" 
+                            buttonStyle={{borderRadius: 10}}
+                        />
+                        <Button
+                            icon={
+                                <FontAwesome
+                                    name="search"
+                                    solid
+                                    size={19}
+                                    style={{marginRight:5}}
+                                    color='#FDB10E'
+                                />
+                            }
+                            title='Find similars'
+                            type="outline" 
+                            buttonStyle={{borderRadius: 10}}
+                        />
+                    </View>                    
                 </View>
             </Card>
         </View>
@@ -70,7 +119,14 @@ export default function Exercises(props)
 
     return (
         <SafeAreaView style={styles.container}>
-            <Title>List of exercises</Title>
+            <Title style={styles.catalogTitle}>Exercises Catalog</Title>
+            <Text>Filters</Text>
+            <View flexDirection='row' justifyContent='space-around' marginBottom='10'>
+                <Text>Body part</Text>
+                <Text>Muscle</Text>
+                <Text>Type</Text>
+                <Text>Not done</Text>
+            </View>
             <FlatList
                 data={DATA}
                 renderItem={renderItem}
@@ -84,13 +140,22 @@ const styles = StyleSheet.create({
     cardContainer: {
         borderRadius: 10
     },
+    catalogTitle: {
+        color: "#FDB10E",
+        fontSize: 28,
+        paddingLeft: 20,
+        alignSelf: 'center'
+    },
     image: {
-        borderRadius: 10
+        borderRadius: 10,
+        resizeMode: 'stretch'
     },
     musclesContainer: {
         flexDirection: "row",
         justifyContent: "space-between",
-        marginTop: 10
+        marginTop: 10,
+        paddingLeft: 5,
+        paddingRight: 5
     },
     muscleContainer: {
         flexDirection: "row",
@@ -99,5 +164,10 @@ const styles = StyleSheet.create({
     muscleItemIcon: {
         marginRight: 5,
         marginBottom: 5
+    }, 
+    cardTitle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
   });
--- components/today/TodayTraining.js
+++ components/today/TodayTraining.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, Dimensions, Text, ScrollView, Image, View, Button } from "react-native";
+import { StyleSheet, Dimensions, Text, ScrollView, View } from "react-native";
 import Routines from "../common/helpers/Routines";
-import { Card } from "react-native-elements";
+import { Button, Card } from "react-native-elements";
 import ExerciseInProgress from '../exercises/ExerciseInProgress';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export const SLIDER_WIDTH = Dimensions.get('window').width + 80
 export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7)
@@ -60,7 +61,7 @@ export default function TodayTraining(props)
                         {exercises.map(exercise => 
                             <Card containerStyle={styles.cardContainer} key={exercise.id}>
                                 <Card.Title style={styles.header}>{exercise.name}</Card.Title>
-                                <Image
+                                <Card.Image
                                     style={styles.image}
                                     source={{
                                         uri: exercise.image
@@ -68,8 +69,36 @@ export default function TodayTraining(props)
                                 />
                                 {!exercise.done ? 
                                     <View style={styles.actionButtons}>
-                                        <Button style={{color: '#007AFF' }} title="Start" onPress={() => setSelectedExercise(exercise)}/>
-                                        <Button style={{color: '#007AFF' }} title="Skip" onPress={() => skipCurrentExercise()}/>
+                                        <Button 
+                                            icon={
+                                                <Icon
+                                                    
+                                                    name="play-circle"
+                                                    size={19}
+                                                    color="#FDB10E"
+                                                    style={{marginRight:10}}
+                                                />
+                                            }
+                                            type="outline" 
+                                            buttonStyle={{borderRadius: 10}}
+                                            title="Start" 
+                                            onPress={() => setSelectedExercise(exercise)}
+                                        />
+                                        <Button 
+                                            icon={
+                                                <Icon
+                                                
+                                                    name="times-circle"
+                                                    size={19}
+                                                    color="#FDB10E"
+                                                    style={{marginRight:10}}
+                                                />
+                                            }
+                                            type="outline"
+                                            title="Skip" 
+                                            buttonStyle={{borderRadius: 10}}
+                                            onPress={() => skipCurrentExercise()}
+                                        />
                                     </View> :
                                     (<Text style={styles.header}>Completed!!</Text>)
                                 }
@@ -100,25 +129,24 @@ const styles = StyleSheet.create({
     },
     cardContainer: {
       borderRadius: 10,
-      height: 350,
-      borderColor: '#169A13',
       borderWidth: 5
     },
     image: {
-      width: '90%',
-      height: 210,
-      resizeMode: 'contain',
-      alignSelf: 'center'
+        borderRadius: 10,
+       height: 210,
+       resizeMode: 'stretch',
+       alignSelf: 'center'
     },
     header: {
       color: "#FDB10E",
-      fontSize: 28,
+      fontSize: 22,
       paddingLeft: 20
     },
     actionButtons: {
-      flexDirection: 'row',
       justifyContent: 'space-between',
-      paddingLeft: 20,
-      paddingRight: 20 
+      height: 85,
+      width: 150,
+      alignSelf: 'center',
+      marginTop: 10
     }
   });
